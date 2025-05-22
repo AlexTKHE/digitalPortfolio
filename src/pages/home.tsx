799638c75f0fb410abfafccfdece5189f45f6648
Alex Thompson <alexthompson28@yahoo.com>
2025-05-21 22:36:41 -0700

    added animations and began with actual section structures

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,20 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
 
-    return <div> Home Page</div>;
+    return <div className = "min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme tog */}
+        <ThemeToggle />
+        {/* Background sec */}
+        <StarBackground />
+        {/* Nav */}
+
+        {/* Content */}
+
+
+        {/* ending part */}
+
+    </div>;
 
 } ;
