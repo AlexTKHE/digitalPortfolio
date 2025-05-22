b971ab9551a4cebd859493ce40c773e4a036567e
Alex Thompson <alexthompson28@yahoo.com>
2025-05-22 11:49:59 -0700

    added navbar and fixed animations, need to fix spacing of nav bar

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -30,11 +30,17 @@ export const StarBackground = () =>
 
     useEffect(() => {
         generateStars();
-    }, []);
-
-    useEffect(() => {
         generateMeteors();
-    })
+
+        const handleResize = () => {
+            generateStars();
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+
+    }, []);
 
     const generateStars = () => {
         const numberOfStars = Math.floor(window.innerWidth * window.innerHeight / 5000);
@@ -64,9 +70,9 @@ export const StarBackground = () =>
             newMeteors.push({
                 id:i,
                 size: Math.random() * 2 + 1,
-                x: Math.random()*100,
+                x: Math.random()*50 + 0.25,
                 y: Math.random()*20,
-                delay: Math.random()* 15,
+                delay: Math.random()* 0.5,
                 animationDuration: Math.random() * 3 + 3,
             });
         }
@@ -88,15 +94,14 @@ export const StarBackground = () =>
         ))}
         
         {meteors.map((meteor) => (
-            <div key={meteor.id} className="meteor animate-meteor" style={{
+           <div key={meteor.id} className="meteor animate-meteor" style={{
                 width: meteor.size*50 + "px",
                 height: meteor.size*2 + "px",
                 left: meteor.x + "%",
                 top: meteor.y + "%",
                 animationDelay: meteor.delay + "s",
                 animationDuration: meteor.animationDuration + "s",
-
-            }}></div>
+            }}></div> 
         ))}
     </div>
 }
